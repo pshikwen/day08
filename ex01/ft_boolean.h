0931f7f6b1f45e261a9a987308d46de01ae194be
Pfumelo Shikweni <pshikwen@c4r13s8.wethinkcode.co.za>
2020-06-26 13:09:40 +0200

    done

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,12 @@
+#include  ft_boolean.h
+
+# define FT_BOOLEAN_H
+# define TRUE 1
+# define FALSE 0
+# define EVEN(x) ((x) % 2)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+# include <unistd.h>
+
